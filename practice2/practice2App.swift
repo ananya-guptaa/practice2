e06f23cc8f04e7bb86d320b1388aa114665e60ad
codermaste <109619670+codermaste@users.noreply.github.com>
2023-06-23 14:30:30 -0400

    Initial Commit

---
+++ practice2/practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  practice2App.swift
+//  practice2
+//
+//  Created by Ananya Gupta on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
